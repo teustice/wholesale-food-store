2f364f28e68617c0e10b696f2ac428a529031414
Tanner Eustice <teustice@msn.com>
2017-06-30 10:48:47 -0700

    seed reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
 Product.destroy_all
+Review.destroy_all
 
 50.times do |index|
-  Product.create!(name: Faker::Food.ingredient,
+  newProduct = Product.create!(name: Faker::Food.ingredient,
                         cost: Faker::Number.decimal(2),
                         origin: Faker::LordOfTheRings.location)
+
+  newProduct.reviews.create(author: Faker::LordOfTheRings.character, content: Faker::HarryPotter.quote, rating: Faker::Number.between(1, 5))
+  newProduct.reviews.create(author: Faker::LordOfTheRings.character, content: Faker::HarryPotter.quote, rating: Faker::Number.between(1, 5))
+  newProduct.reviews.create(author: Faker::LordOfTheRings.character, content: Faker::HarryPotter.quote, rating: Faker::Number.between(1, 5))
+  newProduct.reviews.create(author: Faker::LordOfTheRings.character, content: Faker::HarryPotter.quote, rating: Faker::Number.between(1, 5))
+  newProduct.reviews.create(author: Faker::LordOfTheRings.character, content: Faker::HarryPotter.quote, rating: Faker::Number.between(1, 5))
+  newProduct.reviews.create(author: Faker::LordOfTheRings.character, content: Faker::HarryPotter.quote, rating: Faker::Number.between(1, 5))
+  newProduct.reviews.create(author: Faker::LordOfTheRings.character, content: Faker::HarryPotter.quote, rating: Faker::Number.between(1, 5))
+  newProduct.reviews.create(author: Faker::LordOfTheRings.character, content: Faker::HarryPotter.quote, rating: Faker::Number.between(1, 5))
 end
 
 p "Created #{Product.count} products"
+p "Created #{Review.count} reviews"
