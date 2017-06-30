71d6d4d5658453b50c467a2ab0bc724a291425ad
Tanner Eustice <teustice@msn.com>
2017-06-30 09:43:28 -0700

    seed products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+  has_many :reviews
   validates :name, :presence => true
   validates :cost, :presence => true
   validates :origin, :presence => true
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ActiveRecord::Base
+  belongs_to :product
   validates :author, :presence => true
   validates :content, :presence => true
   validates :rating, :presence => true
